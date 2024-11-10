94fc45db47bac1a0a9803e7100aa4e58b4e2eb11
Julia Terry <156032724+tuliajerry@users.noreply.github.com>
2024-11-10 15:11:05 -0600

    Update index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,9 @@
 const auth = require("./auth");
 const user = require("./user");
+const song = require("./song");
 
 module.exports = {
   auth,
   user,
-};
-
-
-module.exports = {
-  song: require('./song'), 
-  
+  song,
 };
