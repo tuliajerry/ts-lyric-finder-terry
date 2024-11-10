8277602d74a00dbd0b7b8bb40bb5ab8cd40c4928
Julia Terry <156032724+tuliajerry@users.noreply.github.com>
2024-11-10 14:51:04 -0600

    Update htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -20,4 +20,12 @@ router.get("/private", checkAuth, ({ session: { isLoggedIn } }, res) => {
   res.render("protected", { isLoggedIn });
 });
 
+router.get('/', ({ session: { isLoggedIn }, query: { error } }, res) => {
+  res.render('index', { isLoggedIn, error });
+});
+
+router.get('/lyrics', ({ session: { isLoggedIn } }, res) => {
+  res.render('lyrics', { isLoggedIn });
+});
+
 module.exports = router;
