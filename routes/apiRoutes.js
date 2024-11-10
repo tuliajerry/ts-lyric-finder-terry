cbdc5184f3ead3d654a5c8a3f95ae890e8da59de
Julia Terry <156032724+tuliajerry@users.noreply.github.com>
2024-11-10 14:50:40 -0600

    Update apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,13 +2,17 @@ const router = require("express").Router();
 const controllers = require("../controllers");
 const checkAuth = require("../middleware/auth");
 
-// admin login/logout
+
 router.post("/login", controllers.auth.login);
 router.get("/logout", controllers.auth.logout);
 router.post("/signup", controllers.user.create);
 
-// routes/apiRoutes.js
+
 router.get("/songs", controllers.song.getAll);
 router.post("/songs", controllers.song.create);
+
+
+router.get('/search', controllers.lyric.searchLyrics);
+
 
 module.exports = router;
