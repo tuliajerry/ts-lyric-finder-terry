ae63dcf03a0c918c984a70c5601ec31fc104c2b3
Julia Terry <156032724+tuliajerry@users.noreply.github.com>
2024-11-21 16:54:35 -0600

    Update htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,31 +1,17 @@
 const router = require("express").Router();
+const controllers = require("../controllers");
 const checkAuth = require("../middleware/auth");
 
-
-router.get("/", ({ session: { isLoggedIn }, query: { error } }, res) => {
-  res.render("index", { isLoggedIn, error });
+router.get("/", ({ session: { isLoggedIn } }, res) => {
+  res.render("index", { isLoggedIn });
 });
-
-
-router.get("/login", (req, res) => {
-  if (req.session.isLoggedIn) return res.redirect("/");
-  res.render("login", { error: req.query.error });
-});
-
-
-router.get("/signup", (req, res) => {
-  if (req.session.isLoggedIn) return res.redirect("/");
-  res.render("signup", { error: req.query.error });
-});
-
 
 router.get("/private", checkAuth, ({ session: { isLoggedIn } }, res) => {
   res.render("protected", { isLoggedIn });
 });
 
-
-router.get("/lyrics", checkAuth, ({ session: { isLoggedIn } }, res) => {
-  res.render("lyrics", { isLoggedIn });
+router.get('/lyrics', ({ session: { isLoggedIn } }, res) => {
+  res.render('lyrics', { isLoggedIn });
 });
 
 module.exports = router;
