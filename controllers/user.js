18979f0d0830f75f77d9ca7b715119a5f2520048
Julia Terry <156032724+tuliajerry@users.noreply.github.com>
2024-11-21 16:35:21 -0600

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -7,7 +7,7 @@ async function create(req, res) {
     if (!username || !password)
       return res.redirect("/signup?error=must include username and password");
 
-    const user = await User.create(username, password);
+    const user = await User.create({ username, password });
 
     if (!user) return res.redirect("/signup?error=error creating new user");
 
